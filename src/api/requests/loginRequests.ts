4953febf084c89e66e825a1aff045d959af3cb00
luizeduardotesta <luizsonecazzz@hotmail.com>
2024-04-20 21:39:49 -0300

    change the code to no use axios

--- src/api/requests/loginRequests.ts
+++ src/api/requests/loginRequests.ts
@@ -1,15 +1,15 @@
-// apiHelpers.ts
-import axios, { AxiosResponse } from 'axios';
 import { apiUrl } from '../config/variables';
 
-export async function login(email: string, password: string): Promise<AxiosResponse<any>> {
-    try {
-        const response = await axios.post(`${apiUrl}/login`, {
+export async function login(request: any, email: string, password: string) {
+    const response = await request.post(`${apiUrl}/login`, {
+        data: {
             mail: email,
-            password: password
-        });
-        return response;
-    } catch (error) {
-        return error.response;
-    }
+            password: password,
+        },
+        headers: {
+            "Accept": "application/json"
+        }
+    });
+
+    return response;
 }
--- src/api/tests/loginTest.spec.ts
+++ src/api/tests/loginTest.spec.ts
@@ -4,29 +4,29 @@ import { validEmail, validPassword, invalidEmail, invalidPassword } from '../con
 
 export let sysadminToken: string;
 
-test("Success login with sysadmin", async () => {
-    const response = await login(validEmail, validPassword);
+test("Success login with sysadmin", async ({ request }) => {
+    const response = await login(request, validEmail, validPassword);
 
-    expect(response.status).toEqual(200);
-    const responseBody = response.data;
+    expect(response.status()).toEqual(200);
+    const responseBody = await response.json();
     expect(responseBody.token).toBeTruthy();
     expect(responseBody.user.accessProfile).toEqual('SYSADMIN');
 
     sysadminToken = responseBody.token;
 });
 
-test("Error login with sysadmin wrong mail", async () => {
-    const response = await login(invalidEmail, validPassword);
+test("Error login with sysadmin wrong mail", async ({ request }) => {
+    const response = await login(request, invalidEmail, validPassword);
 
-    expect(response.status).toEqual(400);
-    const responseBody = response.data;
+    expect(response.status()).toEqual(400);
+    const responseBody = await response.json();
     expect(responseBody.alert).toEqual("E-mail ou senha informados são inválidos.");
 });
 
-test("Error login with sysadmin wrong password", async () => {
-    const response = await login(validEmail, invalidPassword);
+test("Error login with sysadmin wrong password", async ({ request }) => {
+    const response = await login(request, validEmail, invalidPassword);
 
-    expect(response.status).toEqual(400);
-    const responseBody = response.data;
+    expect(response.status()).toEqual(400);
+    const responseBody = await response.json();
     expect(responseBody.alert).toEqual("E-mail ou senha informados são inválidos.");
 });
